0d738489a47ede3ded98ec3e63a59a0aee1b495b
Jack_McKing <mactavishking@gmail.com>
2018-12-26 17:46:06 +0800

    1. Add a setting page to setup server url and server port 2. Add a class to handle config file 3. bugfix

---
+++ src/main/python/MainUI.py
@@ -0,0 +1,87 @@
+﻿# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\pachongui.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(432, 364)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setGeometry(QtCore.QRect(20, 10, 391, 321))
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab_3 = QtWidgets.QWidget()
+        self.tab_3.setObjectName("tab_3")
+        self.listWidget = QtWidgets.QListWidget(self.tab_3)
+        self.listWidget.setGeometry(QtCore.QRect(10, 10, 351, 241))
+        self.listWidget.setObjectName("listWidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.tab_3)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 260, 271, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton = QtWidgets.QPushButton(self.tab_3)
+        self.pushButton.setGeometry(QtCore.QRect(290, 260, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.tabWidget.addTab(self.tab_3, "")
+        self.tab_4 = QtWidgets.QWidget()
+        self.tab_4.setObjectName("tab_4")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.tab_4)
+        self.lineEdit_2.setGeometry(QtCore.QRect(90, 30, 161, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.tab_4)
+        self.lineEdit_3.setGeometry(QtCore.QRect(300, 30, 41, 20))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.tab_4)
+        self.lineEdit_4.setGeometry(QtCore.QRect(90, 70, 161, 20))
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.label = QtWidgets.QLabel(self.tab_4)
+        self.label.setGeometry(QtCore.QRect(30, 30, 51, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.tab_4)
+        self.label_2.setGeometry(QtCore.QRect(260, 30, 41, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.tab_4)
+        self.label_3.setGeometry(QtCore.QRect(30, 70, 41, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.tab_4)
+        self.label_4.setGeometry(QtCore.QRect(260, 70, 41, 16))
+        self.label_4.setObjectName("label_4")
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.tab_4)
+        self.lineEdit_5.setGeometry(QtCore.QRect(300, 70, 41, 20))
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.pushButton_2 = QtWidgets.QPushButton(self.tab_4)
+        self.pushButton_2.setGeometry(QtCore.QRect(30, 110, 111, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label_5 = QtWidgets.QLabel(self.tab_4)
+        self.label_5.setGeometry(QtCore.QRect(30, 150, 311, 20))
+        self.label_5.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_5.setObjectName("label_5")
+        self.tabWidget.addTab(self.tab_4, "")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(1)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Send"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "Chat"))
+        self.label.setText(_translate("MainWindow", "Address:"))
+        self.label_2.setText(_translate("MainWindow", "Port:"))
+        self.label_3.setText(_translate("MainWindow", "Proxy:"))
+        self.label_4.setText(_translate("MainWindow", "Port:"))
+        self.pushButton_2.setText(_translate("MainWindow", "Test and Coonect"))
+        self.label_5.setText(_translate("MainWindow", "No Status"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("MainWindow", "Setting"))
+
--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,17 +1,22 @@
 import json
+import os
 import time
+import sys
 
 from PyQt5.QtCore import QThread, pyqtSignal
 from fbs_runtime.application_context import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import requests as req
+from configparser import ConfigParser
 
-import sys
+from MainUI import Ui_MainWindow
 
-from ChatRoomUI import Ui_MainWindow
+# TODO Replace this line with "from src.main.python.MainUI import Ui_MainWindow" before coding
 
-SERVER_URL = "http://127.0.0.1"
-SERVER_PORT = "5000"
+SERVER_URL = ""
+SERVER_PORT = ""
+PROXY_URL = ""
+PROXY_PORT = ""
 msgLen = 0
 
 
@@ -42,9 +47,11 @@ class RefreshThread(QThread):
 
     def run(self):
         while True:
-            li = refresh_list()
-            self.trigger.emit(li)
-            time.sleep(1)
+            url = SERVER_URL
+            if url.__contains__("http"):
+                li = refresh_list()
+                self.trigger.emit(li)
+                time.sleep(1)
 
 
 class Dlog(QMainWindow, Ui_MainWindow):
@@ -52,11 +59,60 @@ class Dlog(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(QMainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+
+        # Config parser
+        conf = Configer()
+        conf_li = conf.getConfig()
+        self.lineEdit_2.setText(conf_li[0])
+        self.lineEdit_3.setText(conf_li[1])
+        self.lineEdit_4.setText(conf_li[2])
+        self.lineEdit_5.setText(conf_li[3])
+
         global lw
         lw = self.listWidget
         self.pushButton.pressed.connect(self.puch_btn)
 
+        self.pushButton_2.pressed.connect(self.push_connect_btn)
+
         self.show()
+
+    def push_connect_btn(self):
+        self.label_5.setText("connecting...")
+
+        server_url = self.lineEdit_2.text()
+        server_port = self.lineEdit_3.text()
+        proxy_url = self.lineEdit_4.text()
+        proxy_port = self.lineEdit_5.text()
+
+        if str(proxy_url) is "" or str(proxy_port) is "":
+            try:
+                r = req.get(server_url + ":" + server_port + "/test_connect")
+            except:
+                self.label_5.setText("connect failed")
+        else:
+            proxies = {
+                "http": proxy_url + ":" + proxy_port
+            }
+            try:
+                r = req.get(server_url + ":" + server_port + "/test_connect", proxies=proxies)
+            except:
+                self.label_5.setText("connect failed")
+
+        if str(r.text).__contains__("success"):
+            self.label_5.setText("connect success")
+            self.listWidget.clear()
+            global SERVER_URL
+            global SERVER_PORT
+            global PROXY_URL
+            global PROXY_PORT
+            SERVER_URL = server_url
+            SERVER_PORT = server_port
+            PROXY_URL = proxy_url
+            PROXY_PORT = proxy_port
+            conf = Configer()
+            conf.setConfig()
+        else:
+            self.label_5.setText("connect failed")
 
     def puch_btn(self):
         str = self.lineEdit.text()
@@ -80,28 +136,102 @@ class Dlog(QMainWindow, Ui_MainWindow):
 def send_to_server(text):
     j_data = '{"0":"' + text + '"}'
     send_data = json.loads(j_data)
-    r = req.post(SERVER_URL + ":" + SERVER_PORT + "/put_history", json=send_data)
-    if r.text.__contains__("success"):
-        return True
+
+    if SERVER_URL is "" or SERVER_PORT is "":
+        pass
     else:
-        return False
+        if PROXY_URL is "" or PROXY_PORT is "":
+            r = req.get(SERVER_URL + ":" + SERVER_PORT + "/put_history", json=send_data)
+        else:
+            proxies = {
+                "http": PROXY_URL + ":" + PROXY_PORT
+            }
+            r = req.post(SERVER_URL + ":" + SERVER_PORT + "/put_history", proxies=proxies, json=send_data)
+        if r.text.__contains__("success"):
+            return True
+        else:
+            return False
 
 
 def refresh_list():
     global msgLen
     global isNewMsg
-    r = req.get(SERVER_URL + ":" + SERVER_PORT + "/get_history")
-    r = json.loads(str(r.text))
-    ret = []
-    isNewMsg = False if msgLen is len(r) else True
-    msgLen = len(r)
-    for i in range(len(r)):
-        add_text = r[str(i)]
-        str(add_text).encode("utf-8")
-        add_text = add_text[:-1]  # 去掉\n
-        ret.append(add_text)
-        # self.listWidget.insertItem(self.listWidget.count(), add_text)
-    return ret
+
+    if SERVER_URL is "" or SERVER_PORT is "":
+        pass
+    else:
+        if PROXY_URL is "" or PROXY_PORT is "":
+            r = req.get(SERVER_URL + ":" + SERVER_PORT + "/get_history")
+        else:
+            proxies = {
+                "http": PROXY_URL + ":" + PROXY_PORT
+            }
+            r = req.get(SERVER_URL + ":" + SERVER_PORT + "/get_history", proxies=proxies)
+        r = json.loads(str(r.text))
+        ret = []
+        isNewMsg = False if msgLen is len(r) else True
+        msgLen = len(r)
+        for i in range(len(r)):
+            add_text = r[str(i)]
+            str(add_text).encode("utf-8")
+            add_text = add_text[:-1]  # 去掉\n
+            ret.append(add_text)
+            # self.listWidget.insertItem(self.listWidget.count(), add_text)
+        return ret
+
+
+class Configer:
+
+    def __init__(self):
+        self.server_url = SERVER_URL
+        self.server_port = SERVER_PORT
+        self.proxy_url = PROXY_URL
+        self.proxy_port = PROXY_PORT
+
+        if not self.checkIsConfigExist():
+            f = open(os.getcwd().replace("\\", "/")+"/config.ini", "w", newline="")
+            templete = "[SERVER]\n" \
+                       "url=\n" \
+                       "port=\n" \
+                       "\n" \
+                       "[PROXY]\n" \
+                       "url=\n" \
+                       "port=\n"
+            f.write(templete)
+            f.close()
+        self.PATH = os.getcwd().replace("\\", "/")+"/config.ini"
+
+    def checkIsConfigExist(self):
+        cur_path_files = os.listdir(os.getcwd().replace("\\", "/"))
+        for file in cur_path_files:
+            if os.path.isfile(file):
+                if file == "config.ini":
+                    return True
+        return False
+
+    def getConfig(self):
+        cfg = ConfigParser()
+        print(self.PATH)
+        cfg.read(self.PATH)
+        global SERVER_URL
+        global SERVER_PORT
+        global PROXY_URL
+        global PROXY_PORT
+        SERVER_URL = cfg.get("SERVER", "url")
+        SERVER_PORT = cfg.get("SERVER", "port")
+        PROXY_URL = cfg.get("PROXY", "url")
+        PROXY_PORT = cfg.get("PROXY", "port")
+        return cfg.get("SERVER", "url"), cfg.get("SERVER", "port"), cfg.get("PROXY", "url"), cfg.get("PROXY", "port")
+
+    def setConfig(self):
+        cfg = ConfigParser()
+        cfg.read(self.PATH)
+        cfg.set("SERVER", "url", self.server_url)
+        cfg.set("SERVER", "port", self.server_port)
+        cfg.set("PROXY", "url", self.proxy_url)
+        cfg.set("PROXY", "port", self.proxy_port)
+        f = open(self.PATH, 'w')
+        cfg.write(f)
 
 
 if __name__ == '__main__':
